cb8106f604de60f7470c9c123233d425e490707b
black <hugosousa0804@gmail.com>
2022-06-21 14:48:49 +0100

    Trabalho final

--- CodigoLogins.py
+++ CodigoLogins.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkcalendar import DateEntry #pip install tkcalendar <- para instalar a biblioteca
-from Base_dados import conexao_sql
+#from Base_dados import conexao_sql
 
 #Janela de register
 def ecra3(): #variaveis das Entrys: nome, apelido, nickname, email, data, password, confirmpass, erro
@@ -24,26 +24,29 @@ def ecra3(): #variaveis das Entrys: nome, apelido, nickname, email, data, passwo
     tk.Label(window2, text="User:", font=("", 15)).place(x=50, y=130)
     nickname = tk.Entry(window2, width=25)
     nickname.place(x=102, y=137)
-    tk.Label(window2, text="Email:", font=("", 15)).place(x=350, y=130)
+    tk.Label(window2, text="Email:", font=("", 15)).place(x=50, y=170)
     email = tk.Entry(window2, width=35)
-    email.place(x=410, y=137)
-    tk.Label(window2, text="Data de nascimento:", font=("", 15)).place(x=50, y=170)
+    email.place(x=410, y=175)
+    tk.Label(window2, text="Data de nascimento:", font=("", 15)).place(x=50, y=210)
     data = DateEntry(window2,width=25)
     data.place(x=237, y=175)
-    tk.Label(window2, text="Palavra-passe:", font=("", 15)).place(x=50, y=210)
+    tk.Label(window2, text="Palavra-passe:", font=("", 15)).place(x=50, y=250)
     pass_str = tk.StringVar()
+    confirm = tk.StringVar()
     password = tk.Entry(window2, width=25, show="*", textvariable=pass_str)
-    password.place(x=186, y=216)
-    tk.Label(window2, text="Confirme a palavra-passe:", font=("", 15)).place(x=50, y=250)
-    confirmpass = tk.Entry(window2, width=25, show="*", textvariable=pass_str)
-    confirmpass.place(x=287, y=256)
+    password.place(x=186, y=256)
+    tk.Label(window2, text="Confirme:", font=("", 15)).place(x=350, y=250)
+    confirmpass = tk.Entry(window2, width=25, show="*", textvariable=confirm)
+    confirmpass.place(x=439, y=256)
     a = tk.IntVar(value=0)
     #Função do checkbutton mostrarpass
     def mostarpass():
         if (a.get()==1):
-            password.config(show='') and confirmpass.config(show='')
+            password.config(show='')
+            confirmpass.config(show='')
         else:
-            password.config(show='*') and confirmpass.config(show='*')
+            password.config(show='*')
+            confirmpass.config(show='*')
 
     mostarpass = tk.Checkbutton(window2, text="Mostrar palavra-passe", variable=a, onvalue=1, offvalue=0, command=mostarpass)
     mostarpass.place(x=50, y=290)
@@ -52,15 +55,36 @@ def ecra3(): #variaveis das Entrys: nome, apelido, nickname, email, data, passwo
     erro = tk.Label(window2, text="", fg="red", font=("", 15)) #Label para expor algum tipo de erro
     erro.place(x=50, y=360)
 
-    # Inserir valores na base dados
+    # Inserir valores na base dados e sistema de erro de register
     def Inserir_BD():
+        a = len(nome.get())
+        b = len(apelido.get())
+        c = len(nickname.get())
+        d = len(email.get())
+        e = len(password.get())
+        f = len(confirmpass.get())
+        if a==0:
+            print("Erro 1")
+        elif b==0:
+            print("Erro 2")
+        elif c==0:
+            print("Erro 3")
+        elif d==0:
+            print("Erro 4")
+        elif e==0:
+            print("Erro 5")
+        elif f==0:
+            print("Erro 6")
+        else:
+            pass
+
 
         # print(nome.get())
-        cursor = conexao_sql()
-        comando = f'''INSERT INTO users(nome, apelido, nickname, email,password,data)
-            VALUES ('{nome.get()}','{apelido.get()}','{nickname.get()}','{email.get()}','{password.get()}','{data.get()}')'''
-        cursor.execute(comando)
-        cursor.commit()
+        #cursor = conexao_sql()
+        #comando = f'''INSERT INTO users(nome, apelido, nickname, email,password,data)
+            #VALUES ('{nome.get()}','{apelido.get()}','{nickname.get()}','{email.get()}','{password.get()}','{data.get()}')'''
+        #cursor.execute(comando)
+        #cursor.commit()
 
     tk.Button(window2, text="Criar conta", font=("", 15),command=Inserir_BD).place(x=330, y=400)
     tk.Button(window2, text="iniciar sessão em alternativa", font=("", 15), command=login2).place(x=250, y=440)
@@ -70,9 +94,6 @@ def ecra3(): #variaveis das Entrys: nome, apelido, nickname, email, data, passwo
 def criarconta():
     window.destroy()
     ecra3()
-
-#sistema de erro de register
-
 
 #Janela de login (parte do Rui)
 def ecra2():
@@ -104,8 +125,3 @@ tk.Label(window, text="Deseja iniciar sessão ou criar uma nova conta?", font=("
 tk.Button(window, text="Iniciar sessão", font=("",15), command=login).place(x=150,y=400)
 tk.Button(window, text="Criar conta", font=("",15), command=criarconta).place(x=520,y=400)
 window.mainloop()
-
-
-
-
-

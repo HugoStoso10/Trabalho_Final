cfe7eeb8ff967dd5e53a2696046ae8bf3dffcc9a
black <hugosousa0804@gmail.com>
2022-06-24 02:04:37 +0100

    Trabalho final

--- main.py
+++ main.py
@@ -7,51 +7,56 @@ from PIL import ImageTk, Image
 #Janela de register
 def register(): #variaveis das Entrys: nome, apelido, nickname, email, data, password, confirmpass, erro
     window2 = tk.Tk()
-    window2.geometry("800x600")
+    window2.geometry("1280x720")
     window2.resizable(0,0)  #impede a alteração do tamanho da janela
+    imagem2 = Image.open('imagens\\bg_janela2.jpeg')
+    imagem2 = ImageTk.PhotoImage(imagem2)
+    bg2 = tk.Label(window2, image=imagem2)
+    bg2.image = imagem1
+    bg2.pack()
 
     # funcionalidade do botão iniciar sessão em alternativa: fecha janela de criar conta e abre a janela de login
     def login2():
         window2.destroy()
         ecra2()
 
-    window2.title("Criar conta")
-    tk.Label(window2, text="Preencha os seguintes campos para criar uma conta",font=("", 15)).place(x=150, y=30)
-    tk.Label(window2, text="Nome:", font=("", 15)).place(x=50, y=90)
+    window2.title("433 G4M3C3NT3R - Criar conta")
+    tk.Label(window2, text="Preencha os seguintes campos para criar uma conta",font=("", 15), bg="white").place(x=750, y=50)
+    tk.Label(window2, text="Nome:", font=("", 15), bg="white").place(x=730, y=110)
     nome = tk.Entry(window2, width=25)
-    nome.place(x=112, y=97)
-    nomeerro = tk.Label(window2, text="", font=("", 9), fg="red")
-    nomeerro.place(x=50, y=120)
-    tk.Label(window2, text="Apelido:", font=("", 15)).place(x=350, y=90)
+    nome.place(x=792, y=117)
+    nomeerro = tk.Label(window2, text="", font=("", 9), fg="red", bg="white")
+    nomeerro.place(x=730, y=140)
+    tk.Label(window2, text="Apelido:", font=("", 15), bg="white").place(x=970, y=110)
     apelido = tk.Entry(window2, width=25)
-    apelido.place(x=425, y=97)
-    apelidoerro = tk.Label(window2, text="", font=("", 9), fg="red")
-    apelidoerro.place(x=350, y=120)
-    tk.Label(window2, text="Nome de utilizador:", font=("", 15)).place(x=50, y=140)
+    apelido.place(x=1045, y=117)
+    apelidoerro = tk.Label(window2, text="", font=("", 9), fg="red", bg="white")
+    apelidoerro.place(x=970, y=140)
+    tk.Label(window2, text="Nome de utilizador:", font=("", 15), bg="white").place(x=730, y=160)
     nickname = tk.Entry(window2, width=25)
-    nickname.place(x=222, y=147)
-    nickerro = tk.Label(window2, text="", font=("", 9), fg="red")
-    nickerro.place(x=50, y=170)
-    tk.Label(window2, text="Email:", font=("", 15)).place(x=50, y=200)
+    nickname.place(x=902, y=167)
+    nickerro = tk.Label(window2, text="", font=("", 9), fg="red", bg="white")
+    nickerro.place(x=730, y=190)
+    tk.Label(window2, text="Email:", font=("", 15), bg="white").place(x=730, y=220)
     email = tk.Entry(window2, width=35)
-    email.place(x=107, y=205)
-    emailerro = tk.Label(window2, text="", font=("", 9), fg="red")
-    emailerro.place(x=50, y=230)
-    tk.Label(window2, text="Data de nascimento:", font=("", 15)).place(x=50, y=250)
+    email.place(x=787, y=225)
+    emailerro = tk.Label(window2, text="", font=("", 9), fg="red", bg="white")
+    emailerro.place(x=730, y=250)
+    tk.Label(window2, text="Data de nascimento:", font=("", 15), bg="white").place(x=730, y=270)
     data = DateEntry(window2,width=25)
-    data.place(x=237, y=255)
-    tk.Label(window2, text="Palavra-passe:", font=("", 15)).place(x=50, y=280)
+    data.place(x=917, y=275)
+    tk.Label(window2, text="Palavra-passe:", font=("", 15), bg="white").place(x=730, y=300)
     pass_str = tk.StringVar()
     confirm = tk.StringVar()
     password = tk.Entry(window2, width=25, show="*", textvariable=pass_str)
-    password.place(x=186, y=286)
-    passerro = tk.Label(window2, text="", font=("", 9), fg="red")
-    passerro.place(x=50, y=310)
-    tk.Label(window2, text="Confirme:", font=("", 15)).place(x=350, y=280)
+    password.place(x=866, y=306)
+    passerro = tk.Label(window2, text="", font=("", 9), fg="red", bg="white")
+    passerro.place(x=740, y=330)
+    tk.Label(window2, text="Confirme:", font=("", 15), bg="white").place(x=1030, y=300)
     confirmpass = tk.Entry(window2, width=25, show="*", textvariable=confirm)
-    confirmpass.place(x=439, y=286)
-    passconferro = tk.Label(window2, text="", font=("", 9), fg="red")
-    passconferro.place(x=350, y=310)
+    confirmpass.place(x=1119, y=306)
+    passconferro = tk.Label(window2, text="", font=("", 9), fg="red", bg="white")
+    passconferro.place(x=1030, y=330)
     a = tk.IntVar(value=0)
     #Função do checkbutton mostrarpass
     def mostarpass():
@@ -63,13 +68,11 @@ def register(): #variaveis das Entrys: nome, apelido, nickname, email, data, pas
             confirmpass.config(show='*')
 
     mostarpass = tk.Checkbutton(window2, text="Mostrar palavra-passe", variable=a, onvalue=1, offvalue=0, command=mostarpass)
-    mostarpass.place(x=50, y=330)
-    tk.Label(window2, text="Insire uma do seu rosto (Opcional):", font=("", 15)).place(x=50, y=350)
-    #INSERT DE FOTO DO ROSTO
-    confirmpasserro = tk.Label(window2, text="", font=("", 9), fg="red")
-    confirmpasserro.place(x=50, y=310)
-    criadosucesso = tk.Label(window2, text="", font=("", 9), fg="green")
-    criadosucesso.place(x=220, y=410)
+    mostarpass.place(x=730, y=350)
+    confirmpasserro = tk.Label(window2, text="", font=("", 9), fg="red", bg="white")
+    confirmpasserro.place(x=730, y=330)
+    criadosucesso = tk.Label(window2, text="", font=("", 9), fg="green", bg="white")
+    criadosucesso.place(x=820, y=430)
     # Inserir valores na base dados e sistema de erro de register
     def Inserir_BD():
         a, b, c, d, e, f = len(nome.get()), len(apelido.get()), len(nickname.get()), len(email.get()), len(password.get()), len(confirmpass.get())
@@ -122,38 +125,48 @@ def register(): #variaveis das Entrys: nome, apelido, nickname, email, data, pas
             except:  # pois o campo nickname na base dados é unico
                 nickerro.config(text='Nome de utilizador já utilizado')
 
-    tk.Button(window2, text="Criar conta", font=("", 15),command=Inserir_BD).place(x=330, y=440)
-    tk.Button(window2, text="iniciar sessão em alternativa", font=("", 15), command=login2).place(x=250, y=480)
+    tk.Button(window2, text="Criar conta", font=("", 15),command=Inserir_BD).place(x=940, y=460)
+    tk.Button(window2, text="iniciar sessão em alternativa", font=("", 15), command=login2).place(x=860, y=500)
     window2.mainloop()
 
 #------------------------------JANELA LOGIN------------------------------------------------------
 #Janela de login
 def ecra2():
     window3 =tk.Tk()
-    window3.geometry("800x600")
+    window3.geometry("1280x720")
     window3.resizable(0,0)
-    window3.title("Iniciar sessão")
+    imagem3 = Image.open('imagens\\bg_janela3.jpeg')
+    imagem3 = ImageTk.PhotoImage(imagem3)
+    bg3 = tk.Label(window3, image=imagem3)
+    bg3.image = imagem3
+    bg3.pack()
+    window3.title("433 G4M3C3NT3R - Iniciar sessão")
     # funcionalidade do botão de criar sessão: Fecha janela de login e abre janela de criar conta
     def criarconta2():
         window3.destroy()
         register()
-    tk.Label(window3, text="User:", font=(" ", 15)).place(x=50, y=120)
+    tk.Label(window3, text="Utilizador:", font=(" ", 25), bg="white").place(x=800, y=120)
     nickname = tk.Entry(window3, width=25)
-    nickname.place(x=112, y=127)
-    erronick = tk.Label(window3, text="", font=("", 9), fg="red")
-    erronick.place(x=50, y=170)
-    tk.Label(window3, text="Palavra-passe:", font=("", 15)).place(x=50, y=280)
-    pass_str = tk.StringVar()
+    nickname.place(x=950, y=135)
+    erronick = tk.Label(window3, text="", font=("", 9), fg="red", bg="white")
+    erronick.place(x=800, y=170)
+    tk.Label(window3, text="Palavra-passe:", font=("", 25), bg="white").place(x=750, y=250)
     password = tk.Entry(window3, width=25, show="*")
-    password.place(x=186, y=286)
-    erropass = tk.Label(window3, text="", font=("", 9), fg="red")
-    erropass.place(x=50, y=310)
-    confirmpasserro = tk.Label(window3, text="", font=("", 9), fg="red")
-    confirmpasserro.place(x=50, y=310)
-    criadosucesso = tk.Label(window3, text="", font=("", 9), fg="green")
-    criadosucesso.place(x=220, y=410)
-    semsucesso = tk.Label(window3, text='', font=('',9), fg='red')
-    semsucesso.place(x=220, y=410)
+    password.place(x=970, y=265)
+    erropass = tk.Label(window3, text="", font=("", 9), fg="red", bg="white")
+    erropass.place(x=750, y=300)
+    semsucesso = tk.Label(window3, text='', font=('', 9), fg='red', bg="white")
+    semsucesso.place(x=860, y=350)
+    a = tk.IntVar(value=0)
+
+    def mostarpass():
+        if (a.get()==1):
+            password.config(show='')
+        else:
+            password.config(show='*')
+
+    mostarpass = tk.Checkbutton(window3, text="Mostrar palavra-passe", variable=a, onvalue=1, offvalue=0, command=mostarpass)
+    mostarpass.place(x=970, y=300)
 
     def Consultar_BD():
         a, b = len(nickname.get()), len(password.get())
@@ -172,7 +185,7 @@ def ecra2():
             var1 = (nickname.get())
             var2 = (password.get())
             cursor = conexao_sql()
-            comando = """EXEC logins @nickname=?, @password=?"""
+            comando = """EXEC logins @nickname=?, @password=?""" #stored procedure criado na base dados
             cursor.execute(comando, var1, var2)
             resultado = cursor.fetchall()
             if resultado:
@@ -181,8 +194,8 @@ def ecra2():
             else:
                 semsucesso.config(text='User ou Password nao correspondem')
 
-    tk.Button(window3, text="login", font=("", 15), command=Consultar_BD).place(x=20, y=400)
-    tk.Button(window3, text="criar conta", font=("", 15), command=criarconta2).place(x=90, y=400)
+    tk.Button(window3, text="Entrar", font=("", 20), command=Consultar_BD).place(x=820, y=400)
+    tk.Button(window3, text="Criar conta", font=("", 20), command=criarconta2).place(x=960, y=400)
     window3.mainloop()
 
 #------------------------------JANELA MENU DE JOGOS------------------------------------------------------
@@ -191,16 +204,22 @@ def menujogos():
     window4 = tk.Tk()
     window4.geometry("1280x720")
     window4.resizable(0, 0)
-    window4.title("Menu de jogos")
+    imagem4 = Image.open('imagens\\bg_janela4.jpeg')
+    bg4 = ImageTk.PhotoImage(imagem4)
+    bg = tk.Label(window4, image=bg4)
+    bg.image = imagem4
+    bg.pack()
+    window4.title("433 G4M3C3NT3R - Menu de jogos")
     # funcionalidade do botão: fecha janela do menu e abre a janela do jogo da cobra
     def jogocobra():
         window4.destroy()
         janela_jogocobra()
-    tk.Label(window4, text="Menu de Jogos", font=("", 20)).place(x=540, y=30)
+    tk.Label(window4, text="Menu de Jogos", font=("", 20), bg="white").place(x=540, y=30)
     tk.Button(window4, text=("Jogo da Cobra"), font=("", 12),command=jogocobra, width=55, height=15).place(x=90, y=90)
     frame2 = tk.LabelFrame(window4)
     frame2.place(x=650, y=90)
     tk.Button(frame2, text=("Em breve..."), font=("", 12), width=55, height=15).pack()
+    tk.Button(window4, text=("Sair"), font=("", 20), width=10, bg='#EE0000', command=window4.destroy).place(x=1000,y=500)
     window4.mainloop()
 
 #------------------------------JANELA JOGO DA COBRA------------------------------------------------------
@@ -209,35 +228,110 @@ def janela_jogocobra():
     window5 = tk.Tk()
     window5.geometry("1280x720")
     window5.resizable(0, 0)
-    window5.title("Jogo da Cobra")
+    imagem5 = Image.open('imagens\\bg_janela5.jpeg')
+    bg5 = ImageTk.PhotoImage(imagem5)
+    bg = tk.Label(window5, image=bg5)
+    bg.image = imagem5
+    bg.pack()
+    window5.title("433 G4M3C3NT3R - Jogo da Cobra")
     # Funcionalidade do botão: fecha a janela do jogo da cobra e abre a janela menu
     def menujogos2():
         window5.destroy()
         menujogos()
-    tk.Button(window5, text="Voltar", font=("", 15),command=menujogos2).place(x=50, y=30)
-    tk.Label(window5, text="Jogo da cobra", font=("", 20)).place(x=560, y=30)
-    leaderboard = tk.LabelFrame(window5, width=500, height=550)
-    leaderboard.place(x=50, y=120)
-    tk.Label(window5, text="Como jogar?", font=("", 15)).place(x=560, y=120)
-    tk.Label(window5, text="Movimentação:", font=("", 15)).place(x=560, y=160)
-    tk.Label(window5, text="Up Arrow - mover para cima", font=("", 15)).place(x=560, y=220)
-    tk.Label(window5, text="Down Arrow - mover para baixo", font=("", 15)).place(x=560, y=250)
-    tk.Label(window5, text="Left Arrow - mover para a esquerda", font=("", 15)).place(x=560, y=280)
-    tk.Label(window5, text="Right Arrow - mover para a direita", font=("", 15)).place(x=560, y=310)
-    tk.Label(window5, text="Objetivo:", font=("",15)).place(x=560, y=370)
-    tk.Label(window5, text="Com a cobra comer o máximo de número de maçãs, sem tocar nas bordas\n do mapa e nem em sim própria.\n Quantas mais maçãs forem comidas, maior é a pontuação.\n Bom Jogo!", font=("", 15)).place(x=560, y=400)
+    tk.Button(window5, text="Voltar", font=("", 15),command=menujogos2, bg="white").place(x=50, y=30)
+    tk.Label(window5, text="Jogo da cobra", font=("", 20), bg="white").place(x=900, y=30)
+    tk.Label(window5, text="ID Jogo: 2", font=("", 15), bg="white").place(x=720, y=120)
+    tk.Label(window5, text="Como jogar?", font=("", 15), bg="white").place(x=720, y=150)
+    tk.Label(window5, text="Movimentação:", font=("", 15), bg="white").place(x=720, y=190)
+    tk.Label(window5, text="Up Arrow - mover para cima", font=("", 15), bg="white").place(x=720, y=220)
+    tk.Label(window5, text="Down Arrow - mover para baixo", font=("", 15), bg="white").place(x=720, y=250)
+    tk.Label(window5, text="Left Arrow - mover para a esquerda", font=("", 15), bg="white").place(x=720, y=280)
+    tk.Label(window5, text="Right Arrow - mover para a direita", font=("", 15), bg="white").place(x=720, y=310)
+    tk.Label(window5, text="Objetivo:", font=("",15), bg="white").place(x=720, y=370)
+    tk.Label(window5, text="Com a cobra comer o máximo de número de maçãs,\n sem tocar nas bordas do mapa e nem em sim própria.\n Quantas mais maçãs forem comidas, maior é a pontuação.\n Bom Jogo!\n Premir Pontuações para adicionar o seu melhor resultado!", font=("", 15), bg="white").place(x=700, y=400)
     #Função do botão - inicia o jogo da cobra
     def jogo():
         from jogo_cobra import jogar
         jogar()
-    tk.Button(window5, text="Iniciar", font=("", 15), command=jogo). place(x=870, y=600)
+    tk.Button(window5, text="Iniciar", font=("", 15), command=jogo). place(x=920, y=600)
+    tk.Button(window5, text="Pontuações", font=("", 15), command=pontos).place(x=900, y=550)
     window5.mainloop()
+
+
+#---------------------------------JANELA PONTUAÇÕES-------------------------------------------------------
+
+def pontos():
+    window6 = tk.Tk()
+    window6.geometry("500x620")
+    window6.resizable(0, 0)
+    window6.title("433 G4M3C3NT3R - Pontuações")
+    tk.Label(window6, text="HighScore", font=("", 20)).place(x=30, y=30)
+    id_jogo = tk.Entry(window6, width=25)
+    id_jogo.place(x=200, y=285)
+    erroidjogo = tk.Label(window6, text="", font=("", 9), fg="red")
+    erroidjogo.place(x=200, y=320)
+    nickname = tk.Entry(window6, width=25)
+    nickname.place(x=200, y=365)
+    erronick = tk.Label(window6, text="", font=("", 9), fg="red")
+    erronick.place(x=200, y=400)
+    pontuacao = tk.Entry(window6, width=25)
+    pontuacao.place(x=200, y=445)
+    erropontuacao = tk.Label(window6, text="", font=("", 9), fg="red")
+    erropontuacao.place(x=200, y=480)
+    inseridosucesso = tk.Label(window6, text="", font=("", 9), fg="green")
+    inseridosucesso.place(x=125, y=560)
+
+    def Inserir_BD2():
+        a, b, c = len(nickname.get()), len(pontuacao.get()), len(id_jogo.get())
+        # Erro1 - nickname não foi inserido
+        if a == 0:
+            erronick.config(text="O nickname não foi inserido")
+        else:
+            erronick.config(text="")
+        # Erro2 - password não foi inserido
+        if b == 0:
+            erropontuacao.config(text="A Pontuação não foi inserida")
+        else:
+            erropontuacao.config(text="")
+        if c == 0:
+            erroidjogo.config(text="O id do jogo nao foi inserido")
+        else:
+            erroidjogo.config(text="")
+
+        if (a != 0) and (b != 0) and (c != 0):
+            try:
+                cursor = conexao_sql()
+                comando = f'''INSERT INTO pontuacao(nickname,id_jogo,pontuacao)
+                    VALUES ('{nickname.get()}','{id_jogo.get()}','{pontuacao.get()}')'''
+                cursor.execute(comando)
+                cursor.commit()
+                inseridosucesso.config(text='Pontuação inserida com sucesso')
+            except:
+                erronick.config(text="O nickname não existe na Base de Bados")
+    def top1():
+        cursor = conexao_sql()
+        comando = '''SELECT TOP 1 nickname,pontuacao from pontuacao ORDER BY pontuacao DESC'''
+        cursor.execute(comando)
+        resultado=cursor.fetchall()
+        cursor.commit()
+        return resultado
+    a=top1()
+    tk.Label(window6, text=a, font=("", 15)).place(x=100, y=100)
+    tk.Button(window6, text="Inserir Pontos", font=("", 15),command=Inserir_BD2).place(x=150, y=520)
+    tk.Label(window6, text="Jogo:", font=("", 15)).place(x=100, y=280)
+    tk.Label(window6, text="Nickname:", font=("", 15)).place(x=100, y=360)
+    tk.Label(window6, text="Pontos:", font=("", 15)).place(x=100, y=440)
 
 #------------------------------JANELA BOAS VINDAS------------------------------------------------------
 #janela de boas vindas
 window = tk.Tk()
 window.geometry("800x600")
 window.resizable(0,0) #impede a alteração do tamanho da janela
+imagem1 = Image.open('imagens\\bg_janela1.jpeg')
+bg1 = ImageTk.PhotoImage(imagem1)
+bg = tk.Label(window, image=bg1)
+bg.image = imagem1
+bg.pack()
 #Funcionalidade do botão de criar conta: Fecha a janela de boas vindas e abre a janela de criar conta
 def register2():
     window.destroy()
@@ -247,9 +341,7 @@ def login():
     window.destroy()
     ecra2()
 
-window.title("Bem Vindo a app 'NomedaApp'") #Falta definir nome da App, caso seja para definir 1.
-tk.Label(window, text="Bem vindo à App!", font=("", 20)).place(x=300,y=150) #texto de boas vindas
-tk.Label(window, text="Deseja iniciar sessão ou criar uma nova conta?", font=("",20)).place(x=120,y=200)
-tk.Button(window, text="Iniciar sessão", font=("",15), command=login).place(x=150,y=400)
-tk.Button(window, text="Criar conta", font=("",15), command=register2).place(x=520,y=400)
+window.title("433 G4M3C3NT3R")
+tk.Button(window, text="Iniciar sessão", font=("",15), command=login).place(x=210,y=370)
+tk.Button(window, text="Criar conta", font=("",15), command=register2).place(x=475,y=370)
 window.mainloop()
